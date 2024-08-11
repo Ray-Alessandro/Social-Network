019145ece88ba734fe2eb98b2a4f0fe76112e196
Ray <ray_punkeki@hotmail.com>
2024-08-10 20:29:35 -0500

    feat: first commit

---
+++ src/main/java/dev/raydelcarmen/Technical_Test_Api/Application.java
@@ -0,0 +1,13 @@
+package dev.raydelcarmen.Technical_Test_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/raydelcarmen/Technical_Test_Api/TechnicalTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.raydelcarmen.Technical_Test_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
